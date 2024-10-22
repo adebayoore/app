4fbdf20be984e8f48de8dea7dc45584a09cb0ded
Adebayo <57604357+adebayoore@users.noreply.github.com>
2024-10-22 12:05:32 -0500

    Create marineenv.js

---
+++ src/marineenv.js
@@ -0,0 +1,5 @@
+const consts = {    MDB_CSV: "csv",
+    MDB_MARINE_CLAIMS: "marine_claims",
+    MDB_MARINE_ENTITIES: "marine_entities",
+    MDB_MARINE_VESSELS: "marine_vessels",
+    MDB_MARINE_INSURED: "marine_insured"}
